896ca02acef01b8f29173ccc9732ee882ebb89f4
08kentaro <michaelthearchangel5075@yahoo.co.jp>
2020-01-14 19:57:11 +0900

    アクションをコントローラに定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.all
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image)
   end
 end
